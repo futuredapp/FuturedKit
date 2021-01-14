2512dc8bb85d6414ce5ee4f907a17704025704eb
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2021-01-14 20:26:52 +0100

    Make resource extension inlinable

--- Sources/FuturedKit/Publisher+Resource.swift
+++ Sources/FuturedKit/Publisher+Resource.swift
@@ -1,6 +1,7 @@
 import Combine
 
 extension Publisher {
+    @inlinable
     public func resource() -> Publishers.Catch<Publishers.Map<Self, Resource<Output, Failure>>, Just<Resource<Output, Failure>>> {
         map { output in
             Resource(content: output)
