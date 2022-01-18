4389c84cd25f623182aadf8f4f41cf0c661723eb
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2022-01-18 15:50:53 +0100

    Add `AnyShape` documentation

--- Sources/FuturedKit/AnyShape.swift
+++ Sources/FuturedKit/AnyShape.swift
@@ -1,12 +1,34 @@
 import SwiftUI
 
+/// A type-erased shape.
+///
+/// ## Overview
+///
+/// An `AnyShape` allows changing the type of shape used in a given view
+/// hierarchy. You can use it when different shapes are presented in the same
+/// place but using `AnyView` is too broad since it is not possible to apply
+/// `Shape` related modifiers.
+///
+/// ```swift
+/// var body: some Shape {
+///     (isCapsule ? AnyShape(Capsule()) : AnyShape(Rectangle()))
+///         .stroke()
+/// }
+/// ```
 public struct AnyShape: Shape {
     private let path: (CGRect) -> Path
 
+    /// Create an instance that type-erases a shape.
+    /// - Parameter shape: Concrete type conforming to `Shape`.
     public init<S>(_ shape: S) where S: Shape {
         self.path = shape.path
     }
 
+    /// Describes this shape as a path within a rectangular frame of reference.
+    ///
+    /// - Parameter rect: The frame of reference for describing this shape.
+    ///
+    /// - Returns: A path that describes this shape.
     public func path(in rect: CGRect) -> Path {
         self.path(rect)
     }
