899b1d78538989057965e5cea43b2ff0556b834a
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2022-01-18 14:17:05 +0100

    Fix long generic type lint error

--- Sources/FuturedKit/Publisher+Resource.swift
+++ Sources/FuturedKit/Publisher+Resource.swift
@@ -2,7 +2,9 @@ import Combine
 
 extension Publisher {
     @inlinable
-    public func resource() -> Publishers.Catch<Publishers.Map<Self, Resource<Output, Failure>>, Just<Resource<Output, Failure>>> {
+    public func resource() -> Publishers.Catch<
+        Publishers.Map<Self, Resource<Output, Failure>>, Just<Resource<Output, Failure>>
+    > {
         map { output in
             Resource(content: output)
         }
