8e07062a04628c25339b1e59e21e022e7b9b7341
Michal Martinů <misanovy@gmail.com>
2022-08-29 16:46:00 +0200

    Add equatable tests

--- Tests/FuturedKitTests/ActionTests.swift
+++ Tests/FuturedKitTests/ActionTests.swift
@@ -17,4 +17,36 @@ final class ActionTests: XCTestCase {
         let action: Action<Error> = .loading
         XCTAssertTrue(action.isLoading)
     }
+
+    func testIsLoadingEqual() {
+        let action: Action<Error> = .loading
+        XCTAssertEqual(action, action)
+    }
+
+    func testIsSuccessEqual() {
+        let action: Action<Error> = .success
+        XCTAssertEqual(action, action)
+    }
+
+    func testIsInactiveEqual() {
+        let action: Action<Error> = .inactive
+        XCTAssertEqual(action, action)
+    }
+
+    func testIsFailureEqual() {
+        let action: Action<Error> = .failure(MockError.failed)
+        XCTAssertEqual(action, action)
+    }
+
+    func testIsActionNotEqualSuccessAndLoading() {
+        let action1: Action<Error> = .success
+        let action2: Action<Error> = .loading
+        XCTAssertNotEqual(action1, action2)
+    }
+
+    func testIsActionNotEqualFailureAndInactive() {
+        let action1: Action<Error> = .failure(MockError.failed)
+        let action2: Action<Error> = .inactive
+        XCTAssertNotEqual(action1, action2)
+    }
 }
