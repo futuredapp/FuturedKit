b5081dc217321cf70d2328babbe03eada570cd59
Mikolas Stuchlik <mikolas.stuchlik@futured.app>
2024-08-23 13:29:44 +0200

    Remove legacy code which was not removed by accident

--- Sources/FuturedHelpers/Helpers/FuturedLibraryContentProvider.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-@available(macOS 11, iOS 14, watchOS 7, tvOS 14, *)
-struct FuturedLibraryContentProvider: LibraryContentProvider {
-    var views: [LibraryItem] {
-        LibraryItem(AnyShape(Circle()))
-    }
-}
