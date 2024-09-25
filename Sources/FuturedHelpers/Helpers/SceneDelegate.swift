9c3a4150a56e81fd0fab06fef479c0d3c7a19184
Šimon Šesták <sestak>
2024-09-25 13:48:24 +0200

    Fix access level of SceneDelegate

--- Sources/FuturedHelpers/Helpers/SceneDelegate.swift
+++ Sources/FuturedHelpers/Helpers/SceneDelegate.swift
@@ -8,11 +8,11 @@
 import SwiftUI
 
 #if !os(macOS)
-protocol AppSceneDelegate: AnyObject, UIWindowSceneDelegate, ObservableObject {
+public protocol AppSceneDelegate: AnyObject, UIWindowSceneDelegate, ObservableObject {
     var delegate: SceneDelegate? { get set }
 }
 
-protocol SceneDelegate: AnyObject {
+public protocol SceneDelegate: AnyObject {
     func sceneDidEnterBackground(_ scene: UIScene)
     func sceneWillEnterForeground(_ scene: UIScene)
 }
@@ -37,7 +37,7 @@ extension View {
     /// - Description:
     /// In the main app root view call this modifier and pass the SceneDelegate. You need to specify the AppSceneDelegate which conforms to the UIWindowSceneDelegate.
     /// This is necessary because the SceneDelegate is accessible in SwiftUI only via EnviromentObject.
-    func set<T: AppSceneDelegate>(appSceneDelegateClass: T.Type, sceneDelegate: SceneDelegate) -> some View {
+    public func set<T: AppSceneDelegate>(appSceneDelegateClass: T.Type, sceneDelegate: SceneDelegate) -> some View {
         modifier(SceneDelegateWrapperViewModifier<T>(delegate: sceneDelegate))
     }
 }
