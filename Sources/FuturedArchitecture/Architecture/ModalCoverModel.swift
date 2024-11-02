0a5930bc75f25d04bdb601a597eb59aee60c448b
Šimon Šesták <sestak>
2024-11-02 14:45:59 +0100

    Add detents support in modal presentation of basic views

--- Sources/FuturedArchitecture/Architecture/ModalCoverModel.swift
+++ Sources/FuturedArchitecture/Architecture/ModalCoverModel.swift
@@ -1,21 +1,65 @@
 //
 //  ModalCoverModel.swift
-//  
+//
 //
 //  Created by Simon Sestak on 01/08/2024.
 //
 
+import SwiftUI
 import Foundation
 
-/// Style of the modally presented view. 
+
+public enum SheetDetent: Hashable {
+    case medium
+    case large
+    case height
+    case fraction(CGFloat)
+
+    func detent(size: CGSize) -> PresentationDetent {
+        switch self {
+        case .medium:
+            return .medium
+        case .large:
+            return .large
+        case .height:
+            return .height(size.height)
+        case let .fraction(fraction):
+            return .fraction(fraction)
+        }
+    }
+}
+
+/// Style of the modally presented view.
 ///
 /// It is intended to be used with ``ModalCoverModel``. Style has been placed to
 /// the global scope, since the Model is generic.
 public enum ModalCoverModelStyle {
-    case sheet
+    case sheet(detents: Set<SheetDetent>? = nil)
     #if !os(macOS)
     case fullscreenCover
     #endif
+
+
+    func detents(size: CGSize) -> Set<PresentationDetent>? {
+        if case let .sheet(detents) = self, let detents {
+            return Set(detents.map { $0.detent(size: size) })
+        }
+        return nil
+    }
+
+    var isSheet: Bool {
+        if case .sheet = self {
+            return true
+        }
+        return false
+    }
+
+    var hasDetents: Bool {
+        if case let .sheet(detents) = self {
+            return detents != nil
+        }
+        return false
+    }
 }
 
 /// This struct is a model associating presentation style with a destination on a specific ``Coordinator``.
