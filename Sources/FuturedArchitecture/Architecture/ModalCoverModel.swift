c549e24cc4ee4eae72d01a26f1a39bd595895609
Šimon Šesták <sestak>
2025-01-28 11:36:44 +0700

    Add SheetDetent

--- Sources/FuturedArchitecture/Architecture/ModalCoverModel.swift
+++ Sources/FuturedArchitecture/Architecture/ModalCoverModel.swift
@@ -1,13 +1,35 @@
 //
 //  ModalCoverModel.swift
-//  
+//
 //
 //  Created by Simon Sestak on 01/08/2024.
 //
 
+import SwiftUI
 import Foundation
 
-/// Style of the modally presented view. 
+
+public enum SheetDetent: Hashable {
+    case medium
+    case large
+    case height
+    case fraction(CGFloat)
+
+    func detent(size: CGSize) -> PresentationDetent {
+        switch self {
+        case .medium:
+            return .medium
+        case .large:
+            return .large
+        case .height:
+            return .height(size.height)
+        case let .fraction(fraction):
+            return .fraction(fraction)
+        }
+    }
+}
+
+/// Style of the modally presented view.
 ///
 /// It is intended to be used with ``ModalCoverModel``. Style has been placed to
 /// the global scope, since the Model is generic.
