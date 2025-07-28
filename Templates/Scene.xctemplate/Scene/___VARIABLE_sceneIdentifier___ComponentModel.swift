6ac595e1af682c2a07a1a7ef2c501c8ebc3b50a0
Jakub Marek <jakub.marek@futured.app>
2025-07-28 15:37:52 +0200

    Add @MainActor annotations

--- Templates/Scene.xctemplate/Scene/___VARIABLE_sceneIdentifier___ComponentModel.swift
+++ Templates/Scene.xctemplate/Scene/___VARIABLE_sceneIdentifier___ComponentModel.swift
@@ -9,7 +9,7 @@ protocol ___VARIABLE_sceneIdentifier___ComponentModelProtocol: ComponentModel {
 
 final class ___VARIABLE_sceneIdentifier___ComponentModel: ___VARIABLE_sceneIdentifier___ComponentModelProtocol {
 
-    let onEvent: (Event) -> Void
+    let onEvent: @MainActor (Event) -> Void
 
     private let dataCache: DataCache<DataCacheModel>
 
@@ -35,7 +35,7 @@ extension ___VARIABLE_sceneIdentifier___ComponentModel {
 final class ___VARIABLE_sceneIdentifier___ComponentModelMock: ___VARIABLE_sceneIdentifier___ComponentModelProtocol {
     typealias Event = ___VARIABLE_sceneIdentifier___ComponentModel.Event
 
-    var onEvent: (Event) -> Void = { _ in }
+    var onEvent: @MainActor (Event) -> Void = { _ in }
 
     func onAppear() async {}
 }
